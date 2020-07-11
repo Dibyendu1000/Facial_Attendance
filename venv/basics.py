c3fdd97715a3f2c3eb0695f769b1abbc03ab29f3
Dibyendu Das <43660480+Dibyendu1000@users.noreply.github.com>
2020-07-11 12:09:06 +0530

    Feat: Display Name over Image and Terminal

--- venv/basics.py
+++ venv/basics.py
@@ -2,22 +2,27 @@ import cv2
 import numpy as np
 import face_recognition
 
-imgsubrata1=face_recognition.load_image_file('Face Recog Pro//ImageBasic//subrata1.jpg')
-imgsubrata1=cv2.cvtColor(imgsubrata1,cv2.COLOR_BGR2RGB)
-imgsubrata2=face_recognition.load_image_file('Face Recog Pro//ImageBasic//subrata2.jpg')
-imgsubrata2=cv2.cvtColor(imgsubrata2,cv2.COLOR_BGR2RGB)
+imgTrain=face_recognition.load_image_file('Face Recog Pro//ImageBasic//subhra2.jpg')
+imgTrain=cv2.cvtColor(imgTrain,cv2.COLOR_BGR2RGB)
+imgTest=face_recognition.load_image_file('Face Recog Pro//ImageBasic//subhra1.jpg')
+imgTest=cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
-faceloc1=face_recognition.face_locations(imgsubrata1)[0]
-encodsub1=face_recognition.face_encodings(imgsubrata1)[0]
+faceloc1=face_recognition.face_locations(imgTrain)[0]
+encodeTrain=face_recognition.face_encodings(imgTrain)[0]
 #print(faceloc1)
-cv2.rectangle(imgsubrata1,(faceloc1[3],faceloc1[0]),(faceloc1[1],faceloc1[2]),(0,255,255),2)
+cv2.rectangle(imgTrain,(faceloc1[3],faceloc1[0]),(faceloc1[1],faceloc1[2]),(0,255,255),2)
 
-faceloc2=face_recognition.face_locations(imgsubrata2)[0]
-encodsub2=face_recognition.face_encodings(imgsubrata2)[0]
+faceloc2=face_recognition.face_locations(imgTest)[0]
+encodeTest=face_recognition.face_encodings(imgTest)[0]
 #print(faceloc2)
-cv2.rectangle(imgsubrata2,(faceloc2[3],faceloc2[0]),(faceloc2[1],faceloc2[2]),(255,0,255),2)
+cv2.rectangle(imgTest,(faceloc2[3],faceloc2[0]),(faceloc2[1],faceloc2[2]),(255,0,255),2)
 
+results=face_recognition.compare_faces([encodeTrain],encodeTest)
+faceDis=face_recognition.face_distance([encodeTrain],encodeTest)
+print(results,faceDis)
+#print("sub1:",encodeTrain)
+#print("sub2:",encodeTest)
 
-cv2.imshow('Subrata',imgsubrata1)
-cv2.imshow('Subrata2',imgsubrata2)
+cv2.imshow('Subrata',imgTrain)
+cv2.imshow('Test',imgTest)
 cv2.waitKey(0)
